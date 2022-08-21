d3bb19ca1787c57b14512d1c94074bf24aad2b01
Will Stone <654103+will-stone@users.noreply.github.com>
2022-08-21 15:09:20 +0100

    refactor: inline hints make var args moot

--- src/main/state/store.ts
+++ src/main/state/store.ts
@@ -3,23 +3,21 @@ import { app } from 'electron'
 import { Channel } from '../../shared/state/channels'
 import createStore from '../../shared/state/create-store'
 import { defaultData } from '../../shared/state/reducer.data'
-import type { RootState } from '../../shared/state/reducer.root'
 import { database } from '../database'
 import { actionHubMiddleware } from './middleware.action-hub'
 import { busMiddleware } from './middleware.bus'
 
-const channel = Channel.MAIN
-const middleware = [busMiddleware(), actionHubMiddleware()]
-
-const preloadedState: RootState = {
-  storage: database.getAll(),
-  data: {
-    ...defaultData,
-    version: `${app.getVersion()}${app.isPackaged ? '' : ' DEV'}`,
-    isDefaultProtocolClient: app.isDefaultProtocolClient('http'),
+const { dispatch, getState } = createStore(
+  Channel.MAIN,
+  [busMiddleware(), actionHubMiddleware()],
+  {
+    storage: database.getAll(),
+    data: {
+      ...defaultData,
+      version: `${app.getVersion()}${app.isPackaged ? '' : ' DEV'}`,
+      isDefaultProtocolClient: app.isDefaultProtocolClient('http'),
+    },
   },
-}
-
-const { dispatch, getState } = createStore(channel, middleware, preloadedState)
+)
 
 export { dispatch, getState }
